38f06934cc335867cfb6f94ce07a82a5feca4e4d
Elain Hu <besthynlove@gmail.com>
2022-04-04 12:14:59 -0400

    m

---
+++ src/com/company/arraylistN1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;  // Import the Collections class
+
+public class arraylistN1 {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        Collections.sort(cars);  // Sort cars
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
