ed28436bc68423bfb7f4bc7591bf69546e2f0821
Elain Hu <besthynlove@gmail.com>
2022-04-05 22:19:55 -0400

    m

--- src/com/company/stackRember.java
+++ src/com/company/stackRember.java
@@ -1,89 +1,59 @@
 package com.company;
+
+import java.io.*;
 import java.util.*;
 
-public class stackRember {
-    private int stackSize;
-    private int[] stackArr;
-    private int top;
-
-    /**
-     * constructor to create stack with size
-     * @param size
-     */
-    public stackRember(int size) {
-        this.stackSize = size;
-        this.stackArr = new int[stackSize];
-        this.top = -1;
-    }
-
-    /**
-     * Adds new entry to the top of the stack
-     * @param entry
-     * @throws Exception
-     */
-    public void push(int entry) throws Exception {
-        if(this.isStackFull()){
-            System.out.println("Stack Overflow");
+class stackRember
+{
+    // Pushing element on the top of the stack
+    static void stack_push(Stack<Integer> stack)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            stack.push(i);
         }
-        System.out.println("Adding: "+entry);
-        this.stackArr[++top] = entry;
     }
 
-    /**
-     * Removes an entry from the top of the stack.
-     * @return
-     * @throws Exception
-     */
-    public int pop() throws Exception {
-        if(this.isStackEmpty()){
-            System.out.println("Stack underflow.");
+    // Popping element from the top of the stack
+    static void stack_pop(Stack<Integer> stack)
+    {
+        System.out.println("Pop Operation:");
+
+        for(int i = 0; i < 5; i++)
+        {
+            Integer y = (Integer) stack.pop();
+            System.out.println(y);
         }
-        int entry = this.stackArr[top--];
-        System.out.println("Removed entry: "+entry);
-        return entry;
     }
 
-    /**
-     * Returns top of the stack without removing it.
-     * @return
-     */
-    public int peek() {
-        return stackArr[top];
+    // Displaying element on the top of the stack
+    static void stack_peek(Stack<Integer> stack)
+    {
+        Integer element = (Integer) stack.peek();
+        System.out.println("Element on stack top: " + element);
     }
 
-    /**
-     * Returns true if the stack is empty
-     * @return
-     */
-    public boolean isStackEmpty() {
-        return (top == -1);
+    // Searching element in the stack
+    static void stack_search(Stack<Integer> stack, int element)
+    {
+        Integer pos = (Integer) stack.search(element);
+
+        if(pos == -1)
+            System.out.println("Element not found");
+        else
+            System.out.println("Element is found at position: " + pos);
     }
 
-    /**
-     * Returns true if the stack is full
-     * @return
-     */
-    public boolean isStackFull() {
-        return (top == stackSize - 1);
-    }
 
-    public static void main(String args[]){
-        stackRember  stack = new stackRember (4);
-        try {
-            stack.push(14);
-            stack.push(82);
-            stack.push(32);
-            stack.push(8);
-            stack.pop();
-            stack.push(3);
-            stack.pop();
-            stack.pop();
-            stack.pop();
-            stack.pop();
-            stack.pop();
-            stack.pop();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    public static void main (String[] args)
+    {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        stack_push(stack);
+        stack_pop(stack);
+        stack_push(stack);
+        stack_peek(stack);
+        stack_search(stack, 2);
+        stack_search(stack, 6);
     }
 }
