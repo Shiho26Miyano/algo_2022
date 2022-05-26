343df1ff79fce9ac42f264824bf84e0b84a2bfb6
Elain Hu <besthynlove@gmail.com>
2022-05-26 15:26:07 -0400

    m

--- src/com/company/testTwoPointer.java
+++ src/com/company/testTwoPointer.java
@@ -4,13 +4,17 @@ import java.util.*;
 public class testTwoPointer{
     public static void main(String args[])
     {
-        int[] nums = {3,1,2,4,6};
+        int[] nums4 = {8,3,2,4};
+       int[] nums = {8,3,2,4};
         int target = 6;
 
 
 
-        int[] nums2 = nums;
+        int[] nums2 = nums.clone();
         Arrays.sort(nums2);
+        System.out.println(nums[0]);
+        System.out.println(nums2[0]);
+        System.out.println(nums4[0]);
         int l = 0;
         int r = nums2.length - 1;
 
@@ -28,15 +32,27 @@ public class testTwoPointer{
 
         int nl = 0;
         int nr = 0;
-        for (int i = 0; i < nums.length; i++){
-            if (nums[i] == nums2[l]) nl = i;
+        for (int i = 0; i < nums4.length; i++){
+            //System.out.println(nums4[i]);
+            if (nums[i] == 2){
+                //System.out.println(i);
+                nl = i;
+            }
             if (nums[i] == nums2[r]) nr = i;
         }
 
+        int[] nums3 = {8,3,2,4};
 
 
-        System.out.println(nl);
-        System.out.println(nr);
+        for (int i = 0; i < nums3.length; i++){
+            if(nums3[i] == 3){
+              //  System.out.println(i);
+            }
+            //System.out.println(nums3[i]);
+
+         }
+
+
 
 
     }
